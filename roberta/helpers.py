03359a95f94145d12208f3d9223cb38fd5228ad7
vin <vinit.ravishankar@gmail.com>
2021-08-26 15:36:42 +0200

    fixed loader

--- roberta/helpers.py
+++ roberta/helpers.py
@@ -1,14 +1,24 @@
 import os
+import json
 import torch
+import pickle
 from fairseq.models.roberta import RobertaModel
 
 
 def load_shuffled_model(path):
     if "nopos" in path:
-        model = RobertaModel()
+        base_model = RobertaModel.from_pretrained(os.path.join(os.path.split(path)[0], "roberta.base.orig"))
+        args, task = base_model.model.args, base_model.task
+        args.no_token_positional_embeddings = True
+        new_model = RobertaModel.build_model(args, task)
+        state_dict = torch.load(os.path.join(path, "model.pt"))
+        new_model.load_state_dict(state_dict['model'])
+        base_model.model = new_model
+        return base_model
+
     else:
         return RobertaModel.from_pretrained(path)
 
 
-model = load_shuffled_model('./models/roberta.base.orig')
+m = load_shuffled_model('./models/roberta.base.nopos')
 print("ok")
