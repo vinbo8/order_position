553a014b1347760866ccc3d597f6fa61273a2299
vin <vinit.ravishankar@gmail.com>
2021-09-02 11:44:49 +0200

    ignore checkpoint dir

--- roberta/fairseq/fairseq_cli/train.py
+++ roberta/fairseq/fairseq_cli/train.py
@@ -68,8 +68,8 @@ def main(cfg: FairseqConfig) -> None:
     np.random.seed(cfg.common.seed)
     utils.set_torch_seed(cfg.common.seed)
 
-    if distributed_utils.is_master(cfg.distributed_training):
-        checkpoint_utils.verify_checkpoint_directory(cfg.checkpoint.save_dir)
+    # if distributed_utils.is_master(cfg.distributed_training):
+    #     checkpoint_utils.verify_checkpoint_directory(cfg.checkpoint.save_dir)
 
     # Print args
     logger.info(cfg)
