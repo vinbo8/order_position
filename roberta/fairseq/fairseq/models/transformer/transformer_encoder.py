8b212ae923503f45fe8742d8a861c4ae4a394d50
vin <vinit.ravishankar@gmail.com>
2021-09-02 12:12:21 +0200

    typo

--- roberta/fairseq/fairseq/models/transformer/transformer_encoder.py
+++ roberta/fairseq/fairseq/models/transformer/transformer_encoder.py
@@ -122,7 +122,7 @@ class TransformerEncoderBase(FairseqEncoder):
             token_embedding = self.embed_tokens(src_tokens)
         x = embed = self.embed_scale * token_embedding
         if self.embed_positions is not None:
-            to_scramble = self.cfg.scramble_postion if self.cfg.scramble_partition == 'all' \
+            to_scramble = self.cfg.scramble_position if self.cfg.scramble_partition == 'all' \
                 else (self.training ^ (self.cfg.scramble_position == 'ft'))
             x = embed + self.embed_positions(src_tokens, scramble=to_scramble)
         if self.layernorm_embedding is not None:
