6efeb28672d17dfe0095f1315a32b9613d9becf9
vqc439 <vqc439@a00552.science.domain>
2021-09-03 11:38:39 +0200

    flags default fix

--- analyse_token_reps.py
+++ analyse_token_reps.py
@@ -1,6 +1,6 @@
 import argparse
 from collections import defaultdict
-import json
+import pickle as p
 import numpy as np
 from sklearn.preprocessing import StandardScaler, normalize
 from sklearn.metrics.pairwise import cosine_similarity
@@ -9,11 +9,11 @@ from utils.analyse_token_reps_utils import cai_analysis
 
 def main():
     parser = argparse.ArgumentParser(description="analyse token embeds");
-    parser.add_argument('-e', "--embeds_path", type=str, deault='ptb');
+    parser.add_argument('-e', "--embeds_path", type=str, default='ptb');
     parser.add_argument('-a', "--analysis_type", type=str, default='cai_2021');
     parser.add_argument('-no', "--no_cntx_limit", type=int, default=100);
     parser.add_argument('-p', "--pca", type=int, default=0);
-    parser.add_argument('-c', "--center",  action='store', dault=False);
+    parser.add_argument('-c', "--center",  action='store', default=False);
     parser.add_argument('--lid', action='store_true', default=False, help='task for compute lid')
     parser.add_argument('--lid_metric', type=str, default='l2', help='metric for lid, choose from l2, cos')
     parser.add_argument('--cluster', action='store_true', default=False, help='task for clustering')
@@ -22,7 +22,7 @@ def main():
     arguments = parser.parse_args();
 
     # load embeds
-    embeds_dict = json.load((open(arguments.embeds_path, 'wb')))
+    embeds_dict = p.load((open(arguments.embeds_path, 'rb')))
 
     #run analysis
     cai_analysis(embeds_dict, arguments)
