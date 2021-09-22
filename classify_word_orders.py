75d41a19c30f44d1fc904924400e363a47aa928b
mhany90 <mostafahany56@gmail.com>
2021-09-22 13:48:14 +0200

    Merge branch 'main' of github.com:vin-ivar/order_position into main
    # Conflicts:
    #	rank_word_orders.py

---
+++ classify_word_orders.py
@@ -0,0 +1,78 @@
+from roberta.helpers import load_shuffled_model
+from collections import defaultdict
+import torch.nn.functional as F
+import argparse
+import torch
+from statistics import mean
+from fairseq.data import Dictionary
+import numpy as np
+import random
+from utils.rand_word_order_utils import ud_load_classify
+from scipy.stats import spearmanr
+from sklearn.linear_model import LogisticRegression
+from sklearn.utils import shuffle
+import math
+
+def classify(args, all_examples, all_labels):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Load pre-trained model (weights) and extract reps
+    roberta = load_shuffled_model(args.model_path)
+    roberta.eval()
+    all_sent_encodings = []
+    for sent_idx, sentence in enumerate(all_examples):
+        with torch.no_grad():
+            tokens = roberta.encode(sentence)
+            features = roberta.extract_features(tokens)
+            features = features.squeeze(0).mean(dim=0)
+            all_sent_encodings.append(features.cpu().detach().numpy())
+
+    # make train / dev / test
+    dev_size = math.ceil(len(all_sent_encodings) / 6)
+    train_features, train_labels = np.vstack(all_sent_encodings[:-dev_size]), all_labels[:-dev_size]
+    dev_features, dev_labels = np.vstack(all_sent_encodings[-dev_size:]), all_labels[-dev_size:]
+    #train and eval
+    print(train_features.shape, "train_features ")
+    clf = LogisticRegression(random_state=42).fit(train_features, train_labels)
+    #clf.predict(dev_features)
+    acc = clf.score(dev_features, dev_labels)
+    print(acc, ": acc")
+
+    #return mean_ppl, all_sent_ppl
+
+def main():
+    parser = argparse.ArgumentParser(description="generate token embeddings from corpus");
+    parser.add_argument('-d', "--dataset_path", type=str);
+    parser.add_argument('-m', "--model_path", type=str);
+    parser.add_argument('-l', "--max_sentence_len", type=int, default=10);
+    parser.add_argument('-p', "--no_perms", type=int, default=1);
+
+
+    arguments = parser.parse_args();
+
+    #model
+    print(arguments.model_path, ' :model')
+    # load dataset
+    dataset_file = open(arguments.dataset_path, 'r').read()
+    # pass to permute function, returns list of lists where inner list is of all perms per sentence
+    all_examples, all_labels, leven_distances_to_orig, bleu_to_orig = ud_load_classify(dataset_file,
+        sentence_len_limit=arguments.max_sentence_len, permutation_no=arguments.no_perms)
+    print(len(all_examples), ' no examples')
+    #shuffle exmaples to mix permed and non
+    all_examples, all_labels = shuffle(np.array(all_examples), np.array(all_labels))
+    #classify
+    _ = classify(arguments, all_examples, all_labels)
+    #compute correlation between ppl and levenstein distance
+    #corr = spearmanr(all_sent_ppl, leven_distances_to_orig)
+    #print(corr, " :correlation of perplexity to leven distance to orig order.")
+    #compute correlation between ppl and bleu-4
+    #corr = spearmanr(all_sent_ppl, bleu_to_orig)
+    #print(corr, " :correlation of perplexity to bleu to orig order.")
+
+
+if __name__ == '__main__':
+    main();
+
+
+
+
