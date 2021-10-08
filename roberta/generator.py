98b959e03eb679f5ee4436e76686f625399b6c52
vin <vinit.ravishankar@gmail.com>
2021-10-08 15:35:08 +0200

    remove numpy

--- roberta/generator.py
+++ roberta/generator.py
@@ -7,14 +7,17 @@ model = load_shuffled_model('models/roberta.base.orig')
 token_bank = list(model.bpe.bpe.decoder.keys())
 
 with open("uniform.bpe", "w") as f:
-    for i in tqdm.tqdm([" ".join([str(j) for j in np.random.choice(token_bank, i)]) for i in np.random.choice(range(5, 30), 100000)]):
+    for i in tqdm.tqdm([" ".join([str(j) for j in random.choices(token_bank, k=i)])
+                        for i in random.choices(range(5, 30), k=100000)]):
         f.write(f"{i}\n")
 
 with open("odd_even.bpe", "w") as f:
     odd_bank = token_bank[:len(token_bank) // 2]
     even_bank = token_bank[len(token_bank) // 2:]
-    odd = [" ".join([str(j) for j in np.random.choice(token_bank, i)]) for i in np.random.choice(range(5, 30, 2), 50000)]
-    even = [" ".join([str(j) for j in np.random.choice(token_bank, i)]) for i in np.random.choice(range(6, 30, 2), 50000)]
+    odd = [" ".join([str(j) for j in random.choice(token_bank, k=i)])
+           for i in random.choices(range(5, 30, 2), k=50000)]
+    even = [" ".join([str(j) for j in random.choice(token_bank, k=i)])
+            for i in random.choices(range(6, 30, 2), k=50000)]
     final = odd + even
     random.shuffle(final)
     for i in final:
