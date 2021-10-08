3333afada58097adac152a0c20efcc0fe863f48c
vin <vinit.ravishankar@gmail.com>
2021-10-08 15:16:15 +0200

    tqdm

--- roberta/generator.py
+++ roberta/generator.py
@@ -1,12 +1,13 @@
 import numpy as np
 import random
 from helpers import load_shuffled_model
+import tqdm
 
 model = load_shuffled_model('models/roberta.base.orig')
 token_bank = list(model.bpe.bpe.decoder.keys())
 
 with open("uniform.bpe", "w") as f:
-    for i in [" ".join([str(j) for j in np.random.choice(token_bank, i)]) for i in np.random.choice(range(5, 30), 100000)]:
+    for i in tqdm.tqdm([" ".join([str(j) for j in np.random.choice(token_bank, i)]) for i in np.random.choice(range(5, 30), 100000)]):
         f.write(f"{i}\n")
 
 with open("odd_even.bpe", "w") as f:
